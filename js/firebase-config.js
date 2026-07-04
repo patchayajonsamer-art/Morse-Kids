9e95ab13d3d087d348a2314175d73bca388480fa
patchayajonsamer-art <patchayajonsamer@gmail.com>
2026-07-04 23:14:02 +0700

    Add Firebase configuration file

---
+++ js/firebase-config.js
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+  apiKey: "AIza...",
+  authDomain: "morse-kids-xxxxx.firebaseapp.com",
+  projectId: "morse-kids-xxxxx",
+  storageBucket: "morse-kids-xxxxx.appspot.com",
+  messagingSenderId: "123456789",
+  appId: "1:123456789:web:xxx"
+};
